7033cc50a0a972dc7c776a17f118a93f71ed9766
Luis Eduardo <lfdasilva9@gmail.com>
2022-12-01 09:53:40 -0300

    estrutura da rotas e configurações

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -4,11 +4,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AppExplanation from "../pages/AppExplanation";
 import HabitPage from "../pages/HabitPage";
 import Home from "../pages/Home";
-import Start from "../pages/Start";
 
 const Stack = createNativeStackNavigator();
 
-export default function AllPages() {
+export default function HomePage() {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -16,7 +15,6 @@ export default function AllPages() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="AppExplanation" component={AppExplanation} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="HabitPage" component={HabitPage} />
