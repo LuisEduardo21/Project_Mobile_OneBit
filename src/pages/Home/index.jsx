f0a68f27eccef773893b3581028e6a3e0364bcd5
Luis Eduardo <lfdasilva9@gmail.com>
2022-12-12 18:38:01 -0300

    create route GET

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,47 @@
 import { useNavigation } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 import LifeStatus from "../../components/Common/LifeStatus";
 import CreateHabit from "../../components/Home/CreateHabit";
 import EditHabit from "../../components/Home/EditHabit";
 import StatusBar from "../../components/Home/StatusBar";
+import ChangeNavigationService from "../../Services/ChangeNavigationService";
 
-export default function Home() {
+export default function Home({ route }) {
   const navigation = useNavigation();
   const [mindHabit, setMindHabit] = useState();
   const [moneyHabit, setMoneyHabit] = useState();
   const [bodyHabit, setBodyHabit] = useState();
   const [funHabit, setFunHabit] = useState();
 
+  const [robotDaysLife, setRobotDaysLife] = useState();
+  const today = new Date();
+
   function handleNavExplanation() {
     navigation.navigate("AppExplanation");
   }
+
+  useEffect(() => {
+    ChangeNavigationService.checkShowHome(1)
+      .then((showHome) => {
+        const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+        const checkDays =
+          new Date(formDate) - new Date(showHome.appStartData) + 1;
+
+        setRobotDaysLife(checkDays.toString().padStart(2, "0"));
+      })
+      .catch((err) => console.log(err));
+  }, [route.params]);
 
   return (
     <View style={styles.container}>
       <ScrollView>
         <View style={{ alignItems: "center" }}>
-          <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
-
+          <Text style={styles.dailyChecks}>
+            ❤️ {robotDaysLife} {robotDaysLife === "01" ? "dia" : "dias"} - ✔️ 80
+            Checks
+          </Text>
           <LifeStatus />
           <StatusBar />
 
